36fc92d2a6686380eb849a285e799204e1dc8d7a
Infinidoge <sten.erickson04@gmail.com>
2019-02-25 11:36:36 -0500

    Cog Dynamo mostly depreciated and cog base class removed. (Functionality in Discord.py)

--- core/dynamos/cog_dynamo.py
+++ core/dynamos/cog_dynamo.py
@@ -3,22 +3,19 @@ Ralsei Core | Dynamos | Cog Dynamo
 ------------------------------
 Description:
     The Cog Dynamo is the Ralsei Dynamo (or 'engine') for managing cogs themselves.
-    This pertains to adding events through the Event Dynamo, and adding the cog normally.
     For registering commands to a cog without a cog file, the Command Dynamo will employ the Cog Dynamo
-
-    Within this file is also the base cog class, of which all cogs will inherit.
-    This base cog class defines how a cog registers events, etc.
 """
 
 
-class Cog:
-    """
-    The base class of a cog for Ralsei
-    """
-    EventDynamo = None
-    bot = None
+class CogDynamo:
+    def __init__(self, bot):
+        self.bot = bot
+        self.cogs = []
 
-    @classmethod
-    def _init(cls, bot):
-        cls.EventDynamo = bot.dynamos["Events"]
-        cls.bot = bot
+    def add_cog(self, cog):
+        print(f"Cog Loaded: {type(cog).__name__}")
+        self.cogs += [cog]
+
+    def load_cogs(self):
+        for cog in self.cogs:
+            self.bot.add_cog(cog)
